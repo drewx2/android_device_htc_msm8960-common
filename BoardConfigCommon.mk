5f290500448fcbe7392589ea55baa4445e235efb
Drew <drewx2@gmail.com>
2013-03-04 22:26:04 -0800

    wifi fix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,17 +95,7 @@ TARGET_USES_SF_BYPASS := true
 
 # Wifi
 DX2_BUILD_WIFI						:= true
-WPA_SUPPLICANT_VERSION				:= VER_0_8_X
-BOARD_WLAN_DEVICE_REV 				:= bcm4334
-BOARD_HOSTAPD_DRIVER				:= NL80211
-WIFI_DRIVER_FW_PATH_AP 				:= "/system/etc/firmware/fw_bcm4334_apsta.bin"
-WIFI_DRIVER_FW_PATH_STA 			:= "/system/etc/firmware/fw_bcm4334.bin"
-WIFI_DRIVER_FW_PATH_P2P 			:= "/system/etc/firmware/fw_bcm4334_p2p.bin"
-WIFI_DRIVER_MODULE_NAME 			:= "bcmdhd"
-WIFI_DRIVER_MODULE_PATH 			:= "/system/lib/modules/bcmdhd.ko"
-WIFI_DRIVER_FW_PATH_PARAM   		:= "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_MODULE_ARG 				:= "firmware_path=/system/etc/firmware/bcmdhd.bin nvram_path=/etc/calibration"
-WIFI_BAND		 					:= 802_11_ABG
+DX2_WIFI_TYPE						:= wext
 
 # Webkit
 ENABLE_WEBGL := true 
